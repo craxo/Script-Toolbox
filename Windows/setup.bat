a71ffc3827cf7ba431cb4dbe674d770b90396f11
craxo <38186154+craxo@users.noreply.github.com>
2022-04-28 12:46:44 +0200

    Remove pause of script

--- Windows/setup.bat
+++ Windows/setup.bat
@@ -8,6 +8,3 @@ SET DOMAIN=%USERDOMAIN%
 
 :: Move delete-all-desktop-shortcuts script to users windows startup folder
 copy /y "%~dp0delete-all-desktop-shortcuts.bat" "C:\Users\%USERNAME%.%DOMAIN%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
-
-:: Wait for user input before closing terminal
-pause
